049eb56136e7823d7eb31cbfbbbae9d3632bbc54
Yaxi Ye <ss1271@gmail.com>
2016-01-06 23:38:09 +0000

    Remove the front-facing video related code and switched to using proximity sensor

--- PushUp/ViewController.swift
+++ PushUp/ViewController.swift
@@ -21,69 +21,28 @@ class ViewController: UIViewController, AVCaptureVideoDataOutputSampleBufferDele
     
     @IBAction func onResetClick(sender: UIButton) {
         self.counter = 0;
-        dispatch_async(dispatch_get_main_queue()) {
-            self.counterLabel.text = String(format:"%d", self.counter);
-        }
+        self.counterLabel.text = String(format:"%d", self.counter);
     }
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view, typically from a nib.
-        captureSession.sessionPreset = AVCaptureSessionPresetLow;
-        let devices = AVCaptureDevice.devices();
-        
-        for device in devices {
-            if (device.hasMediaType(AVMediaTypeVideo)) {
-                if (device.position == AVCaptureDevicePosition.Front) {
-                    captureDevice = device as? AVCaptureDevice;
-                    if (captureDevice != nil) {
-                        beginSession();
-                    }
-                }
-            }
-        }
-
+        UIDevice.currentDevice().proximityMonitoringEnabled = true;
+        NSNotificationCenter.defaultCenter().addObserver(self, selector: "sensorStateChanged", name: "UIDeviceProximityStateDidChangeNotification", object: nil);
     }
 
     override func didReceiveMemoryWarning() {
         super.didReceiveMemoryWarning()
         // Dispose of any resources that can be recreated.
     }
-
-    func beginSession() {
-        var err : NSError? = nil;
-        do {
-            let input = try AVCaptureDeviceInput(device: captureDevice)
-            captureSession.addInput(input)
-        } catch _ {
-            print("error: \(err?.localizedDescription)");
-        }
-        
-        captureSession.startRunning();
-        let output = AVCaptureVideoDataOutput();
-        output.setSampleBufferDelegate(self, queue: dispatch_queue_create("sample buffer", DISPATCH_QUEUE_SERIAL));
-        captureSession.addOutput(output);
-    }
     
-    func captureOutput(captureOutput : AVCaptureOutput, sampleBuffer : CMSampleBufferRef) {
-        
-    }
-    
-    // protocol implementation
-    
-    func captureOutput(captureOutput: AVCaptureOutput!, didOutputSampleBuffer sampleBuffer: CMSampleBuffer!, fromConnection connection: AVCaptureConnection!) {
-        let metadataDict : Dictionary = CMCopyDictionaryOfAttachments(nil, sampleBuffer, kCMAttachmentMode_ShouldPropagate)! as Dictionary;
-        let exifMetadata : NSMutableDictionary = metadataDict[kCGImagePropertyExifDictionary as NSString] as! NSMutableDictionary;
-        let brightnessValue = exifMetadata[kCGImagePropertyExifBrightnessValue as NSString] as! Float;
-        if (brightnessValue <= 1.0 && !inProgress) {
+    func sensorStateChanged() {
+        if (UIDevice.currentDevice().proximityState == true && !inProgress) {
             inProgress = true;
-        }
-        if (brightnessValue > 1.0 && inProgress) {
+            print("Close to user");
+        } else {
             inProgress = false;
+            print("Not close to user");
             counter++;
-            dispatch_async(dispatch_get_main_queue()) {
-                self.counterLabel.text = String(format:"%d", self.counter);
-            }
-            print(String(format:"brightness value: %.2f", brightnessValue));
+            self.counterLabel.text = String(format:"%d", self.counter);
         }
     }
 }
